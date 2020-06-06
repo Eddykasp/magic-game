a311eb40b274d3701c0b5bfe394e94e7789ba6d5
Max Kasperowski <maxkasperowski@gmail.com>
2020-06-06 17:05:46 +0200

    enable animations for aoe

--- js/aoeSpell.js
+++ js/aoeSpell.js
@@ -13,18 +13,35 @@ var AoE = function (px,py,damage,lifetime){
     return isHit;
   };
   this.damage = damage;
+  this.currentSprite = 0;
 
   this.draw = function(ctx,vp,c1,c2){
-    
-    ctx.fillStyle = c2;
-    ctx.fillRect((this.pos.x - 15) - vp.pos.x,
-    (this.pos.y - 15),
-    30,30);
-    ctx.fillStyle = c1;
-    ctx.fillRect((this.pos.x - 12) - vp.pos.x,
-    (this.pos.y - 12),
-    24,24);
-  }
+    this.sprites[this.currentSprite](this,ctx,vp,c1,c2);
+    this.currentSprite = (this.currentSprite+1)%this.sprites.length;
+  };
+
+  this.sprites = [
+    function(that, ctx,vp,c1,c2){
+      ctx.fillStyle = c2;
+      ctx.fillRect((that.pos.x - 15) - vp.pos.x,
+        (that.pos.y - 15),
+        30,30);
+      ctx.fillStyle = c1;
+      ctx.fillRect((that.pos.x - 12) - vp.pos.x,
+        (that.pos.y - 12),
+        24,24);
+    },
+    function(that, ctx,vp,c1,c2){
+      ctx.fillStyle = c1;
+      ctx.fillRect((that.pos.x - 15) - vp.pos.x,
+        (that.pos.y - 15),
+        30,30);
+      ctx.fillStyle = c2;
+      ctx.fillRect((that.pos.x - 12) - vp.pos.x,
+        (that.pos.y - 12),
+        24,24);
+    }
+  ];
 
   this.move = function (){
     this.framesLeft -= 1;
